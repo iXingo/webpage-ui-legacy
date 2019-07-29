395bd1e22ddcc5ae945c71b296e3957b58bd70cc
Shawn Wang <shawang@nvidia.com>
2019-07-29 10:31:55 +0800

    fixed issues

--- config-overrides.js
+++ config-overrides.js
@@ -5,8 +5,9 @@ module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
     config = rewireLess.withLoaderOptions({
       modifyVars: { 
-          "@layout-body-background": "#FFFFFF",
-          "@layout-header-background": "#FFFFFF",
+          // "@layout-body-background": "#FFFFFF",
+          "@layout-body-background": "##FFFFFF",
+          "@layout-header-background": "#007BFF",
           "@layout-footer-background": "#FFFFFF" 
       },
       javascriptEnabled: true
--- src/Component/NavHeader/index.js
+++ src/Component/NavHeader/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav'
 import './index.css'
-import {Link} from 'react-router-dom';
 
 class NavHeader extends Component {
 
