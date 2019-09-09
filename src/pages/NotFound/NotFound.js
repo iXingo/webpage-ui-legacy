2b85778f934f9236a6d3cde0e004af109f4dcc8d
Shawn Wang <ixingo@126.com>
2019-09-10 00:07:41 +0800

    modified page tips content

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -28,7 +28,7 @@ const NotFound = () => {
         <Grid item lg={6} xs={12}>
           <div className={classes.content}>
             <Typography variant="h1">404: 你来到了知识的荒原</Typography>
-            <Typography variant="subtitle2">您当前访问的页面不存在</Typography>
+            <Typography variant="subtitle2">您当前访问的页面不存在,您已经来到了一个未知的知识荒原。</Typography>
             <Typography variant="subtitle2">此页面从未被创建，或者已经被系统管理员汪师傅永久性或者临时性地删除/转移, 您可以尝试联系汪师傅以获得关于此页面的更多详细帮助。</Typography>
             <img alt="Under development" className={classes.image} src="/images/undraw_page_not_found_su7k.svg"/>
           </div>
