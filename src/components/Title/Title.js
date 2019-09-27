5f1e1617db51a1a947927bb1d553e8d34a2a4d7b
Shawn Wang <ixingo@126.com>
2019-09-28 01:51:56 +0800

    add new component

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -8,16 +8,16 @@ const useStyles = makeStyles(theme => ({
   main: {
     alignItems: 'center',
     justifyContent: 'space-betwwen',
-    padding: theme.spacing(3),
+    padding: '36px 0',
     [theme.breakpoints.up('md')]: {
-      padding: theme.spacing(6),
+      padding: '36px 0',
       paddingRight: 0,
     },
     color: '#1a73e8',
   },
   mainImg: {
-    height: 320,
-    maxHeight: 400,
+    // height: 320,
+    maxHeight: 320,
     padding: 15
 
   },
@@ -29,7 +29,7 @@ const Title = props => {
 
   return (
     <Grid container className={classes.main}>
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12} md={8}>
         <Typography overline gutterBottom variant='h5' style={{padding: '10px 0'}} color='textSecondary'>
           新闻视角
         </Typography>
@@ -38,12 +38,12 @@ const Title = props => {
         </Typography>
         <Typography overline gutterBottom variant='h5' style={{padding: '30px 0'}} color="textSecondary">
           2013年注册第一个域名，申请的第一个主机，配置第一个WordPress程序开始，就一直想从前端和后端搭建写一个博客。 这些年折折腾腾，也没搞出来，当然也没写出什么有意思的东西。 今天，我再次出发，在这个网站（再次）开始我的博客生涯，头也不回的开始我的程序人生。        </Typography>
-        <Button variant="outlined" size="large" color="primary">
+        <Button variant="outlined" size="small" color="primary">
           查看更多
         </Button>
       </Grid>
-      <Grid item md={6}>
-        <img alt="Cover" className={classes.mainImg} src="http://localhost:3000/images/img/undraw_special_event_4aj8.svg" />
+      <Grid item md={4}>
+        <img alt="Cover" className={classes.mainImg} src="https://developers.google.cn/china/images/skyline_microsite_480.png" />
       </Grid>
     </Grid>
   );
