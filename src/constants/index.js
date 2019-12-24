c020b3ad75e34b78a388ed3e36067ab8058d864f
Shawn Wang <ixingo@126.com>
2019-12-24 17:19:34 +0800

    update domain

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://106.12.190.37:8080/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://ixingo.com.cn/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
