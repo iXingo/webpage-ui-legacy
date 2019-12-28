56ad9c2584a62644e6097e69e1274bcd794e8de1
Shawn Wang <ixingo@126.com>
2019-12-28 11:47:04 +0800

    update website meta data

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -21,8 +21,10 @@ const Footer = props => {
   return (
     <div {...rest} className={clsx(classes.root, className)}>
 	  <TypeWriter typing={1}>
-		<Typography variant="body1">
+	  <Typography variant="body1">
 		    [ICP证:  皖ICP备15011098号]{'     '}
+	  </Typography>
+	  <Typography variant="body1">
 			&copy;{' '}
 			<Link component="a" href="http://shawn.wang/" target="_blank" style={{paddingRight: 5}}>
 				Shawn Wang
@@ -38,6 +40,9 @@ const Footer = props => {
 		<Typography variant="caption" display="block">
 			&copy; Copyright 2020. 所有权利保留。本网站由汪师傅进行开发维护， 所有权利与解释权归汪师傅所有， 未经允许或授权， 严禁转载， 复制， 刻录与分发。
 			所有权利是指本网站所上载的信息与内容， 网站源代码不包含在此项权利申明中， 在遵守法律的前提下， 开发者可以在开源许可范围内自由使用， 分发， 修改以及商业使用。
+		</Typography>
+		<Typography variant="caption" display="block">
+			本网站构建并托管于百度智能云BCC， 静态文件使用七牛云进行CDN加速， 短信服务由腾讯云提供支持， DNS域名解析服务由阿里云万网提供， 微信公众号平台基于新浪云SAE进行构建。
 		</Typography>
       </TypeWriter>
     </div>
