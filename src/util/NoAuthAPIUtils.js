7ab466586f799a72d58d6563de4a471a8cc70614
Shawn Wang <ixingo@126.com>
2020-01-25 20:56:11 +0800

    updated api function

--- src/util/NoAuthAPIUtils.js
+++ src/util/NoAuthAPIUtils.js
@@ -18,10 +18,3 @@ const request = (options) => {
           })
       );
 };
-
-// export function getAllPosts() {
-//   return request({
-//     url: API_BASE_URL + "/posts/all",
-//     method: 'GET'
-//   });
-// }
