509726eeccf6282af4bc1dcb55462b643e543954
Shawn Wang <ixingo@126.com>
2020-05-12 23:30:32 +0800

    updated tips

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
 
     notification[notificationType]({
       message: '星狗网 Web App',
-      description: description + "下次再见！亲爱的「" + this.state.currentUser.name + "」同学~",
+      description: description + "下次再见！【" + this.state.currentUser.name + "】同学~",
     });
     localStorage.removeItem(ACCESS_TOKEN);
 
