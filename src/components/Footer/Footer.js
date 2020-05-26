f9c6031d2eb8c98efd01e1c3188472753bed5851
Shawn Wang <ixingo@126.com>
2020-05-26 11:35:26 +0800

    updated footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -88,7 +88,7 @@ const Footer = props => {
 
       <div className={clsx(classes.footlink, className)}>
         <Grid container spacing={4} className={classes.root}>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={6} md={4}>
             <Typography  gutterBottom variant='h6' style={{color: 'black'}}>
               服务指南
             </Typography>
@@ -107,8 +107,14 @@ const Footer = props => {
             <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
               <span role={"img"} aria-label={"商务洽谈"}>🛋</span> <a href={"https://weibo.com/ixingo"} style={{textDecoration: 'underline'}}>商务洽谈</a>
             </Typography>
+            <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
+              <span role={"img"} aria-label={"关于网站"}>🌐️</span> <a href={"/about"} style={{textDecoration: 'underline'}}>关于网站</a>
+            </Typography>
+            <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
+              <span role={"img"} aria-label={"关于站长"}>👨‍🎓️</span> <a href={"/about-me"} style={{textDecoration: 'underline'}}>关于站长</a>
+            </Typography>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={6} md={4}>
             <Typography  gutterBottom variant='h6' style={{color: 'black'}}>
               导航链接
             </Typography>
@@ -131,7 +137,7 @@ const Footer = props => {
               <span role={"img"} aria-label={"博文"}>🏙</span> <a href={"/posts"} style={{textDecoration: 'underline'}}>博文</a>
             </Typography>
           </Grid>
-          <Grid item xs={12} md={4}>
+          <Grid item xs={6} md={4}>
             <Typography  gutterBottom variant='h6' style={{color: 'black'}}>
               社交/媒体
             </Typography>
@@ -142,13 +148,19 @@ const Footer = props => {
               <span role={"img"} aria-label={"LinkedIn"}>📦</span> <a href={"https://www.linkedin.com/in/ixingo/"} style={{textDecoration: 'underline'}}>LinkedIn</a>
             </Typography>
             <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
-              <span role={"img"} aria-label={"StackOverFlow"}>📇</span> <a href={"/index"} style={{textDecoration: 'underline'}}>StackOverFlow</a>
+              <span role={"img"} aria-label={"StackOverFlow"}>📇</span> <a href={"https://stackoverflow.com/users/6028692/ixingo"} style={{textDecoration: 'underline'}}>StackOverFlow</a>
             </Typography>
             <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
               <span role={"img"} aria-label={"牛客网"}>📑</span> <a href={"https://www.nowcoder.com/profile/531527"} style={{textDecoration: 'underline'}}>牛客网</a>
             </Typography>
             <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
               <span role={"img"} aria-label={"哔哩哔哩"}>🧷</span> <a href={"https://space.bilibili.com/4130475"} style={{textDecoration: 'underline'}}>哔哩哔哩</a>
+            </Typography>
+            <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
+              <span role={"img"} aria-label={"知乎"}>🔎️</span> <a href={"https://www.zhihu.com/people/ixingo"} style={{textDecoration: 'underline'}}>知乎</a>
+            </Typography>
+            <Typography  gutterBottom variant='h6' style={{fontSize: 14, fontWeight:400, marginBottom: 2, color: 'black'}} color="textSecondary">
+              <span role={"img"} aria-label={"V2EX"}>🌏️</span> <a href={"https://v2ex.com/member/iXingo"} style={{textDecoration: 'underline'}}>V2EX</a>
             </Typography>
           </Grid>
         </Grid>
