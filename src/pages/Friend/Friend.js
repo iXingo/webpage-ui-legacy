1e9b79f8420e4a6bf9e32750bd741d3e706d46bd
Shawn Wang <ixingo@126.com>
2020-09-16 00:04:55 +0800

    update friend page

--- src/pages/Friend/Friend.js
+++ src/pages/Friend/Friend.js
@@ -5,6 +5,7 @@ import FriendList from './Components/FriendList';
 // import { mergeClasses } from '@material-ui/styles';
 import {makeStyles} from '@material-ui/core/styles';
 import NotAuth from '../NotAuth';
+import ContentTitle from "../../components/ContentTitle/ContentTitle";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,10 +21,30 @@ const Friend = props => {
   document.title = "汪师傅和他的朋友们 | 看见，好时光";
   if (!props.isAuthenticated) return <NotAuth/>;
 
+
+  const SchoolFriend= {
+    sectionMain: '学生时代',
+    sectionTitle: '君子淡如水，岁久情愈真。',
+    sectionContent: '从学生时代相识的旧友伙伴，久经考验的好友。'
+  }
+
+  const SocietyFriend= {
+    sectionMain: '工作生涯',
+    sectionTitle: '山河不足重，重在遇知已。',
+    sectionContent: '初入江湖的新知， 在这山长水远的江湖遇见的同行人。'
+  }
+
   return (
     <div>
       <CssBaseline/>
       <FriendHead {...props}/>
+
+      <ContentTitle context={SchoolFriend}/>
+      <div className={classes.root}>
+        <FriendList {...props}/>
+      </div>
+
+      <ContentTitle context={SocietyFriend}/>
       <div className={classes.root}>
         <FriendList {...props}/>
       </div>
