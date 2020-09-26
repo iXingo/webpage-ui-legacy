2e730a25b6136d7354dcc41c1c92149b22d6519f
Shawn Wang <ixingo@126.com>
2020-09-26 22:22:03 +0800

    update friend page introduction

--- src/pages/Friend/FriendPage.js
+++ src/pages/Friend/FriendPage.js
@@ -218,7 +218,7 @@ const FriendPage = props => {
             <img src={"https://img.ixingo.cn/web/suzhou.jpeg"} alt={''}/>
             <span className={classes.float}>
               <div className={classes.title}>
-                与远见卓识的你一起在生活中勇攀高峰。
+                与远见卓识的你一起，在生活中勇攀高峰。
               </div>
               <div className={classes.content}>
               为 Apple 的创新产品构建坚实基础。作为团队中的一员，你将与世界一流的研究人员交流与合作，利用顶尖计算、存储和分析工具解决机器学习领域中极具挑战性的难题。在 Apple，你的团队可以在硬件、软件、算法等所有领域全面创新，一切机会尽在眼前。工作领域包括后端工程、数据科学、平台工程和系统工程。
@@ -251,7 +251,7 @@ const FriendPage = props => {
 
           <div className={classes.main}>
             <Typography variant="h6" gutterBottom color="textSecondary" align="center" className={classes.firstLine}>
-              开始探索
+              现在，开始探索
             </Typography>
             <Typography variant="h1" gutterBottom color="inherit" align="center" className={classes.secondLine}>
               汪师傅心中的好朋友们
